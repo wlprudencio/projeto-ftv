2af2116e58efd068af8dae84c565f9cf4886c29a
WallacePrudencio <119940315+wlprudencio@users.noreply.github.com>
2026-04-18 02:09:58 -0300

    Atualizações do projeto Ftv

--- src/main/java/com/example/futvolei/servico/UsuarioServico.java
+++ src/main/java/com/example/futvolei/servico/UsuarioServico.java
@@ -3,10 +3,7 @@ package com.example.futvolei.servico;
 import com.example.futvolei.dtos.UsuarioDto;
 import com.example.futvolei.model.Usuario;
 
-public interface UsuarioServico {
-
+public interface UsuarioServico { // Certifique-se que diz INTERFACE aqui
     Usuario findByUsuario(UsuarioDto usuario);
-
     boolean validaUsuario(String email, String senha);
-
 }
